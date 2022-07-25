4787aa713346cba98506f3b01462bfa774063554
Yash Ghatti <yashghatti@gmail.com>
2022-07-25 16:00:34 -0600

    intial commit

---
+++ src/test/java/ca/yashghatti/amqp/AmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.yashghatti.amqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
